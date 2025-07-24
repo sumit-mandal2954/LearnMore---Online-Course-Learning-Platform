baac82efccbbde24d9b2631e912f17a4f187e54d
sumit-mandal2954 <sumitmandal2954@gmail.com>
2025-07-24 14:18:03 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/LearnMore---Online-Course-Learning-Platform/',
   plugins: [react()],
